90c86441b58561e707747b6cc661f688c7f509c3
HospelThiv <serx123@yandex.ru>
2022-05-23 14:29:54 +0400

    feat: performance 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require("fs");
+const path = require('path');
 
+async function readDirFun() {
+
+    const data = await fs.promises.readdir(path.join(__dirname))
+    const dataFiles = await fs.promises.readdir(path.join(__dirname, 'files'))
+
+    if (data.indexOf('files-copy') < 0) {
+        fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+            if (err) { return console.error(err) }
+            // console.log('ok')
+        })
+    }
+
+    const dataCopyFolder = await fs.promises.readdir(path.join(__dirname, 'files-copy'))
+
+    if (dataCopyFolder.length) {
+        dataCopyFolder.forEach(file => {
+            fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+                if (err) throw err; // не удалось удалить файл
+            });
+        });
+    }
+
+    dataFiles.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err => {
+            if (err) throw err; // не удалось скопировать файл
+        });
+    });
+
+}
+
+try {
+    readDirFun();
+} catch (error) {
+    console.log(err);
+}
