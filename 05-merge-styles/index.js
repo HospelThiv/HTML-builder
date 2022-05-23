b566da6387bd05b303799cc0b3c5277b1ac61945
HospelThiv <serx123@yandex.ru>
2022-05-23 22:56:00 +0400

    feat: performance 05-marge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require('path');
+
+async function readDirFun() {
+
+    const data = await fs.promises.readdir(path.join(__dirname, 'project-dist'))
+    const dataFiles = await fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+
+    if (data.indexOf('bundle.css') < 0) {
+        fs.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w', (err) => {
+            if (err) throw err; // не удалось создать файл
+            writeFileCss();
+        });
+    } else {
+        fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), err => {
+            if (err) throw err; // не удалось удалить файл
+        });
+        fs.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w', (err) => {
+            if (err) throw err; // не удалось создать файл
+            writeFileCss();
+        });
+    }
+
+    function writeFileCss() {
+        dataFiles.forEach(file => {
+            if (file.isFile()) {
+                if (file.name.length >= 5 && file.name.substring(file.name.length - 4, file.name.length) == '.css') {
+                    const readStr = fs.createReadStream(path.join(__dirname, 'styles', file.name));
+                    readStr.on("data", function (chunk) {
+                        fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), chunk, (err) => {
+                            if (err) throw err;
+                        });
+                    });
+                }
+            }
+        });
+    }
+}
+
+try {
+    readDirFun();
+} catch (error) {
+    console.log(err);
+}
